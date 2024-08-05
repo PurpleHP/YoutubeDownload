1107e168cde3d88479efed9a32b24cbf45fb5ad1
PurpleHP <ermertay@gmail.com>
2024-08-06 02:24:24 +0300

    youtube dl fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const ytdl = require('ytdl-core');
+const youtubedl = require('youtube-dl-exec');
 const app = express();
 
 app.use(cors());
@@ -14,8 +14,14 @@ app.post('/getMp4', async (req, res) => {
     }
 
     try {
-        const info = await ytdl.getInfo(url);
-        res.json(info);
+        const output = await youtubedl(url, {
+            dumpSingleJson: true,
+            noCheckCertificates: true,
+            noWarnings: true,
+            preferFreeFormats: true,
+            addHeader: ['referer:youtube.com', 'user-agent:googlebot']
+        });
+        res.json(output);
     } catch (error) {
         console.error('Error fetching video info', error);
         res.status(500).json({ error: 'Error fetching video info' });
