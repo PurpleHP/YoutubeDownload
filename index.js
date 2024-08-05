0cf8ac1c40f1f6fba643ac9bacc72022ce687a78
PurpleHP <ermertay@gmail.com>
2024-08-06 02:52:06 +0300

    idk

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const youtubedl = require('youtube-dl-exec');
+const { exec } = require('child_process');
 const app = express();
 
-
 const corsOptions = {
-    origin: 'https://www.merttaylan.dev/', 
+    origin: 'https://www.merttaylan.dev/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204
@@ -15,28 +14,43 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.post('/getMp4', async (req, res) => {
+app.post('/getMp4', (req, res) => {
     const url = req.body.url;
     if (!url) {
         return res.status(400).json({ error: 'Invalid YouTube URL' });
     }
 
-    try {
-        const output = await youtubedl(url, {
-            dumpSingleJson: true,
-            noCheckCertificates: true,
-            noWarnings: true,
-            preferFreeFormats: true,
-            addHeader: ['referer:youtube.com', 'user-agent:googlebot']
-        });
-        res.json(output);
-    } catch (error) {
-        console.error('Error fetching video info', error);
-        res.status(500).json({ error: 'Error fetching video info' });
-    }
+    const command = `youtube-dl ${url} --dump-single-json --no-check-certificates --no-warnings --prefer-free-formats --add-header referer:youtube.com --add-header user-agent:googlebot`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error('Error fetching video info', error);
+            return res.status(500).json({ error: 'Error fetching video info' });
+        }
+
+        try {
+            const output = JSON.parse(stdout);
+            res.json(output);
+        } catch (parseError) {
+            console.error('Error parsing video info', parseError);
+            res.status(500).json({ error: 'Error parsing video info' });
+        }
+    });
 });
 
 const PORT = process.env.PORT || 10000;
+
+exec('python --version', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error fetching Python version: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Python version stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Python version: ${stdout.trim()}`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
