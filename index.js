efaa99f285336b4c76cb697402b07adc852d46b4
PurpleHP <ermertay@gmail.com>
2024-08-06 02:35:10 +0300

    port change

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/getMp4', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
